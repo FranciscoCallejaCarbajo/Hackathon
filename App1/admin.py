ee820b25aa8dd4b382735aa70c5b0518d227f79d
Fran <franciscocallejacarbajo17@gmail.com>
2024-06-12 18:35:20 +0200

    update

--- App1/admin.py
+++ App1/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Usuarios
 
+admin.site.site_header = 'Usuarios'
+admin.site.site_title = 'Usuario'
 # Register your models here.
+@admin.register(Usuarios)
+class UsuariosAdmin(admin.ModelAdmin):
+    fields = ["nombre", "apellido", "email", "contraseña", "birthday"]
+    list_display = ["nombre", "apellido", "email", "contraseña", "birthday"]
+
--- App1/models.py
+++ App1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=50, verbose_name="Nombre", null=False, blank=False)
+    apellido = models.CharField(max_length=50, verbose_name="Apellido", null=False, blank=False)
+    email = models.EmailField(max_length=50, verbose_name="Email", null=False, blank=False)
+    contraseña = models.CharField(max_length=50, verbose_name="Contraseña", null=False, blank=False)
+    birthday = models.DateField(null=False, verbose_name="Cumpleaños", blank=False)
+
+    class Meta:
+        db_table = "Usuarios"
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+    def _str_(self) -> str:
+        return self.nombre
