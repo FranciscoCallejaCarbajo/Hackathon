00f9c6615fb54cac8fdd487d07b30ee930d9641e
FranciscoCallejaCarbajo <160488986+FranciscoCallejaCarbajo@users.noreply.github.com>
2024-06-17 10:08:12 +0200

    update imagenes cursos y likes

--- Hackathon/App1/urls.py
+++ Hackathon/App1/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import IndexView, LoginView, RegisterView, UserView, LogoutView, crear_usuario, like_course
 
@@ -10,3 +12,6 @@ urlpatterns = [
     path('crear_usuario/', crear_usuario, name='crear_usuario'),
     path('like/<int:curso_id>/', like_course, name='like_course'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
